079125ef6cbd90f907eb62c46f9860a770740700
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-03-16 14:56:30 -0500

    add hovering + color change: addEventListener and function to change squares colors to green + restart button to reset the board

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Creating drawing board */
 function grid(x) {
     let board = document.getElementById('board');
     for (let i=0; i<x; i++){
@@ -5,7 +6,7 @@ function grid(x) {
         row.className = 'row';
         for (let j=0; j<x; j++){
             let item = document.createElement('div');
-            item.className = 'square';
+            item.setAttribute('id', 'square');
             row.appendChild(item);
         }
         board.appendChild(row);
@@ -14,3 +15,16 @@ function grid(x) {
 
 grid(16);
 
+/* Click on board starts the hovering function */
+let board = document.getElementById('board');
+board.addEventListener('click', () => {
+    let square = document.querySelectorAll('#square');
+    square.forEach(item => { item.addEventListener('mouseenter', () => {
+        item.setAttribute('class', 'color-change')
+        }); 
+    })
+} );
+
+/* Restart board */
+let reset = document.getElementById('reset');
+reset.addEventListener('click', () => location.reload());
